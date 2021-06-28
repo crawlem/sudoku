e05e7cac61f085477c652132bd0d4af58246882f
Mark Crawley <mark.crawley@specsavers.com>
2021-06-28 11:49:16 +0100

    Changed default font

--- nuxt.config.js
+++ nuxt.config.js
@@ -14,13 +14,17 @@ export default {
       { hid: 'description', name: 'description', content: '' }
     ],
     link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
+      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
+      { rel: 'preconnect', href: 'https://fonts.googleapis.com' },
+      { rel: 'preconnect', href: 'https://fonts.gstatic.com', crossorigin: '' },
+      { rel: 'stylesheet', href: 'https://fonts.googleapis.com/css2?family=Nunito&display=swap' }
     ]
   },
 
   // Global CSS: https://go.nuxtjs.dev/config-css
   css: [
-    '~/assets/scss/colours.scss'
+    '~/assets/scss/colours.scss',
+    '~/assets/scss/typography.scss'
   ],
 
   styleResources: {
